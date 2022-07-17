c6d2f4ed2ae25f8108a6b5a1e3e9ef18c297ad65
jpviegas <jp.viegas@hotmail.com>
2022-07-17 20:56:57 -0300

    feat: form

--- src/pages/NewProject/NewProject.jsx
+++ src/pages/NewProject/NewProject.jsx
@@ -1,7 +1,12 @@
+import ProjectForm from '../../components/project/ProjectForm';
+import './NewProject.css';
+
 function NewProject() {
   return (
     <main className="new-project">
-      <h1>New Project</h1>
+      <h1>Create project:</h1>
+      <p>Create and then update your services</p>
+      <ProjectForm />
     </main>
   );
 }
