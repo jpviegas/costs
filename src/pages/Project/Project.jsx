3ab659eef73811f07cd53665524c7f1a2470a25e
jpviegas <jp.viegas@hotmail.com>
2022-07-25 18:33:52 -0300

    style: project edit page

--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Container from '../../components/Layout/Container/Container';
+import Loading from '../../components/Layout/Loading/Loading';
 import './Project.css';
 
 function Project() {
   const { id } = useParams();
   const [project, setProject] = useState([]);
+  const [showProjectForm, setShowProjectForm] = useState(false);
   const url = 'http://localhost:5000/';
 
   useEffect(() => {
@@ -21,10 +24,43 @@ function Project() {
       .catch((err) => new Error(err));
   }, [id]);
 
+  function toggleProjectForm() {
+    setShowProjectForm(!showProjectForm);
+  }
+
   return (
     <>
-      <p>project</p>
-      <p>{project.name}</p>
+      {project.name ? (
+        <div className="project-details">
+          <Container customClass="column">
+            <div className="details-container">
+              <h1>Project: {project.name}</h1>
+              <button type="button" onClick={toggleProjectForm}>
+                {!showProjectForm ? 'Edit project' : 'Close'}
+              </button>
+              {!showProjectForm ? (
+                <div className="project-info">
+                  <p>
+                    <span>Category:</span>
+                    {project.category.name}
+                  </p>
+                  <p>
+                    <span>Budget</span>R${project.budget}
+                  </p>
+                </div>
+              ) : (
+                <div className="project-info">
+                  <p>Project Form</p>
+                </div>
+              )}
+            </div>
+          </Container>
+        </div>
+      ) : (
+        <Loading />
+      )}
+      {/* <p>project</p> */}
+      {/* <p>{project.name}</p> */}
     </>
   );
 }
